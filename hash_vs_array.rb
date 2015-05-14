271fb760810335ebbbc64198b1374fadc27558b3
Dorian <i.am.eager.for.peace@gmail.com>
2015-05-14 10:18:27 +0900

    ミスったでござる

--- hash_vs_array.rb
+++ hash_vs_array.rb
@@ -2,8 +2,8 @@ require 'benchmark'
 
 TIMES = 100000000
 
-target_hash = {}
 def measure_hash
+  target_hash = {}
   puts Benchmark.measure {
     for i in 0..TIMES do
       target_hash[i.to_s] = i.to_s
@@ -11,8 +11,8 @@ def measure_hash
   }
 end
 
-target_array = []
 def measure_array
+  target_array = []
   puts Benchmark.measure {
     for i in 0..TIMES do
       target_array << i.to_s
