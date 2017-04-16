0b2780379a6ed18e074f2afd546fbc8f40c0bbef
doridoridoriand <i.am.eager.for.peace@gmail.com>
2017-04-16 20:35:51 +0900

    uuidが衝突したらSlackにつぶやくボットスクリプト

---
+++ uuid/check_uniq.rb
@@ -0,0 +1,26 @@
+require 'mysql2'
+require 'net/http'
+require 'uri'
+require 'json'
+
+client = Mysql2::Client.new(:host => 'uuid.c9pxhxbdcuca.ap-northeast-1.rds.amazonaws.com',
+                            :username => 'dorian',
+                            :password => 'password',
+                            :database => 'uuid'
+)
+
+data = client.query("select uuid from uuid.ruby").map {|row| row['uuid']}.to_a
+
+if data.length != data.uniq.length
+  text = 'UUID collision occuerd!!!!'
+else
+  text = '今日もUUIDは平和です'
+end
+
+uri = URI.parse('')
+https = Net::HTTP.new(uri.host, uri.port)
+https.use_ssl = true
+req = Net::HTTP::Post.new(uri.request_uri)
+req['Content-Type'] = 'application/json'
+req.body = {"text" => text}.to_json
+https.request(req)
