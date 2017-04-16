5767bb99e926f36fa8f718b051c93657bab1b194
doridoridoriand <i.am.eager.for.peace@gmail.com>
2017-04-16 19:50:14 +0900

    uuidを生成しDBに突っ込むスクリプト

--- uuid/gen.rb
+++ uuid/gen.rb
@@ -1,6 +1,17 @@
 require 'securerandom'
+require 'mysql2'
+
+client = Mysql2::Client.new(:host => 'uuid.c9pxhxbdcuca.ap-northeast-1.rds.amazonaws.com',
+                            :username => 'dorian',
+                            :password => 'password',
+                            :database => 'uuid'
+)
 
 while true do
-  p SecureRandom.uuid.upcase
-  sleep(1)
+  begin
+    client.query("insert into uuid.ruby (`uuid`) values ('#{SecureRandom.uuid}')")
+    sleep(1)
+  rescue => e
+    p e
+  end
 end
