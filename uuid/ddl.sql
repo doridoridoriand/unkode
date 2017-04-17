9f4c0e3093d7c91e9b5ad49fbfc59870c46e7170
doridoridoriand <i.am.eager.for.peace@gmail.com>
2017-04-17 22:24:49 +0900

    python用のテーブルを用意

--- uuid/ddl.sql
+++ uuid/ddl.sql
@@ -3,3 +3,9 @@ create table `ruby` (
   `uuid` varchar(191),
   primary key(`id`)
 ) engine=InnoDB default charset=utf8;
+
+create table `python` (
+  `id`   bigint(20) unsigned not null auto_increment,
+  `uuid` varchar(191),
+  primary key(`id`)
+) engine=InnoDB default charset=utf8;
