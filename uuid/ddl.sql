dafa2754f037c5d91f3469e56a2465383c599cfe
doridoridoriand <i.am.eager.for.peace@gmail.com>
2017-04-17 23:47:09 +0900

    nodejs用のテーブルを用意

--- uuid/ddl.sql
+++ uuid/ddl.sql
@@ -9,3 +9,9 @@ create table `python` (
   `uuid` varchar(191),
   primary key(`id`)
 ) engine=InnoDB default charset=utf8;
+
+create table `node` (
+  `id`   bigint(20) unsigned not null auto_increment,
+  `uuid` varchar(191),
+  primary key(`id`)
+) engine=InnoDB default charset=utf8;
