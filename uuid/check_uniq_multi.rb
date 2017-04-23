16c6a91a74eb6a59be9dd7fd35e5e64cd3760faf
doridoridoriand <i.am.eager.for.peace@gmail.com>
2017-04-23 10:13:21 +0900

    マルチスレッド版の重複チェックスクリプトを用意

--- uuid/check_uniq_multi.rb
+++ uuid/check_uniq_multi.rb
@@ -22,31 +22,30 @@ client = Mysql2::Client.new(:host => '',
 number_of_data = client.query("select id from uuid.ruby order by id desc limit 1").map {|data| data["id"]}.first
 
 index_arr = (1..number_of_data).to_a
+collision_uuid = []
 
 Parallel.each(index_arr, in_thread: 8) do |id|
-  client_multi = Mysql2::Client.new(:host => '',
-                              :username => 'root',
-                              :password => 'password',
-                              :database => 'uuid'
-                             )
+  uuid = client.query("select uuid from uuid.ruby where id = #{id}").map {|res| res['uuid']}.first
+  count = client.query("select * from uuid.ruby where uuid = #{"'" + uuid + "'"}").map {|res| res['id']}.to_a.count
 
-  uuid = client_multi.query("select uuid from uuid.ruby where id = #{id}").map {|res| res['uuid']}.first
-  p      client_multi.query("select * from uuid.ruby where uuid = #{"'" + uuid + "'"}").map {|res| res}
+  if count.to_i === 1
+    p "#{uuid}: is unique"
+  else
+    collision_uuid << uuid
+  end
 end
 
-#p data.length
-#p data.uniq.length
-#if data.length != data.uniq.length
-#  text = 'UUID collision occuerd!!!!'
-#else
-#  text = "生成されたUUID: #{data.length.to_s}\n今日もUUIDは平和です"
-#end
-#puts text
+if collision_uuid.count != 0
+  text = 'UUID collision occuerd!!!!'
+else
+  text = "生成されたUUID: #{data.length.to_s}\n今日もUUIDは平和です"
+end
+puts text
 
-#uri = URI.parse('')
-#https = Net::HTTP.new(uri.host, uri.port)
-#https.use_ssl = true
-#req = Net::HTTP::Post.new(uri.request_uri)
-#req['Content-Type'] = 'application/json'
-#req.body = {"text" => text}.to_json
-#https.request(req)
+uri = URI.parse('')
+https = Net::HTTP.new(uri.host, uri.port)
+https.use_ssl = true
+req = Net::HTTP::Post.new(uri.request_uri)
+req['Content-Type'] = 'application/json'
+req.body = {"text" => text}.to_json
+https.request(req)
