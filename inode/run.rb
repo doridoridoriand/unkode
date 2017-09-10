a1215efbe11eeadcc001a817f11183001e675785
doridoridoriand <i.am.eager.for.peace@gmail.com>
2017-09-10 17:41:30 +0900

    出力回数を間引いた。ループ回数を本番仕様に

--- inode/run.rb
+++ inode/run.rb
@@ -14,14 +14,16 @@ Net::SSH.start(host, user) do |session|
   10.times {|i|
     sub_directory = "#{base_directory}/#{i}"
     session.exec!("mkdir #{sub_directory}") if session.exec!("ls #{sub_directory}").include?('No such file or directory')
-    10.times {
+    1000000.times {|j|
       uuid = SecureRandom.uuid
       filename_with_path = "#{base_directory}/#{i}/#{uuid}"
       session.exec!("touch #{filename_with_path}")
-      res = session.exec!("df -i")
-      inodes = res.split(/\n/).map {|line| line if line.include?('dev')}.compact!.last.split(' ')
-      current_inodes = {inodes: inodes[1], iused: inodes[2], ifree: inodes[3], percent: inodes[4]}
-      logger.info("Used: #{current_inodes[:iused]}  Remain: #{current_inodes[:ifree]}  Percentage: #{(current_inodes[:ifree].to_f / current_inodes[:inodes].to_f).to_s[0..5]}%")
+      if j % 10 == 0
+        res = session.exec!("df -i")
+        inodes = res.split(/\n/).map {|line| line if line.include?('dev')}.compact!.last.split(' ')
+        current_inodes = {inodes: inodes[1], iused: inodes[2], ifree: inodes[3], percent: inodes[4]}
+        logger.info("Used: #{current_inodes[:iused]}  Remain: #{current_inodes[:ifree]}  Percentage: #{(current_inodes[:ifree].to_f / current_inodes[:inodes].to_f).to_s[0..5]}%")
+      end
     }
   }
 end
