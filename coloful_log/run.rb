733677869052b0b614fc4803779a7afdbb4ffb44
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-01-02 21:34:09 +0900

    ターミナルで使用できる色を全て表示するスクリプト

---
+++ coloful_log/run.rb
@@ -0,0 +1,15 @@
+require 'pry'
+#e.g. https://tech.mktime.com/entry/183
+
+# Only running on terminal with 256bit color support.
+(1..255).map {|n| puts "\e[38;5;#{n}mああああああああああああああああああああああああああああああああああああああああああああ\e[0m"; sleep(0.5)}
+
+# Running with terminal only supports 8 and 16bit color.
+(1..8).to_a.map {|set|
+  ((31..37).to_a << 39 << (90..97).to_a).flatten.map {|txt|
+    ((40..47).to_a << 49 << (100..107).to_a).flatten.map {|bg|
+      puts "\e[#{set};#{txt};#{bg}mああああああああああああああああああああああああああああああああああああああああああああ\e[0m"
+      sleep(0.5)
+    }
+  }
+}
