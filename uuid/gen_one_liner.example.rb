d17bcd818b0477ae6d18ec74e896b86435675c75
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-02-15 11:19:01 +0900

    シェルとして実行できるように変更

--- uuid/gen_one_liner.example.rb
+++
@@ -1,1 +0,0 @@
-require 'securerandom'; uuid = []; while true do; begin; uuid << SecureRandom.uuid; rescue => e; end; end
---
+++ uuid/gen_one_liner.rb.sh
@@ -0,0 +1,1 @@
+ruby -e "require 'securerandom'; uuid = []; while true do; begin; uuid << SecureRandom.uuid; rescue => e; end; end"
