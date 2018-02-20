7a95ce438dd2c5403fa22a117f5879366a10cb29
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-02-20 14:12:13 +0900

    文法揺れが合ったのでちょっと修正

--- uuid/gen_on_memory.js
+++ uuid/gen_on_memory.js
@@ -1,8 +1,8 @@
 // 実行時オプションに --max-old-space-size=4096 などと付けないとV8の1.5GB制限に引っかかってまともにメモリ消費できないので注意
 
 let uuid = require('node-uuid');
-let uuids = []
+let uuids = [];
 
 while (true) {
-  uuids.push(uuid.v4())
+  uuids.push(uuid.v4());
 }
