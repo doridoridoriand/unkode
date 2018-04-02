24752ae1280b1522c93b1c4c4f175ad8443c7b93
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-04-03 02:35:39 +0900

    純粋にRubyの世界で完結出来る計算内容に切り替えられるようにした

--- parallel/run.rb
+++ parallel/run.rb
@@ -7,11 +7,13 @@ OPTIONS = {}
 OptionParser.new do |opt|
   opt.on('-n number_of_calc_times', '--number_of_calc_times', Integer) {|v| OPTIONS[:calc_times]   = v}
   opt.on('-t type_of_calc',         '--type_of_calc',         String)  {|v| OPTIONS[:type_of_calc] = v}
+  opt.on('-c calcuration_method',   '--calcuration_method',   String)  {|v| OPTIONS[:calc_method]  = v}
   opt.parse!(ARGV)
 end
 
-raise OptionParser::MissigArgument, 'NoCalcurationRepeatsSpecified' unless OPTIONS[:calc_times]
-raise OptionParser::MissigArgument, 'NoCalcurationTypeDetected'     unless OPTIONS[:type_of_calc]
+raise OptionParser::MissingArgument, 'NoCalcurationRepeatsSpecified' unless OPTIONS[:calc_times]
+raise OptionParser::MissingArgument, 'NoCalcurationTypeDetected'     unless OPTIONS[:type_of_calc]
+raise OptionParser::MissingArgument, 'NoCalcurationMethodDetected'   unless OPTIONS[:calc_method]
 
 arr = [*0..OPTIONS[:calc_times]]
 
@@ -21,7 +23,29 @@ elsif OPTIONS[:type_of_calc] === 'process'
   type = {in_process: 24}
 end
 
-uuids = Parallel.map(arr, type) {|i|
-  [SecureRandom.uuid, i]
-}
+public
+
+def uuid
+  [SecureRandom.uuid]
+end
+
+def arr
+  a = [*0..100]
+  b = [101..200]
+  a.map {|i|
+    b.map {|j|
+      i + j
+    }
+  }.inject(:+)
+end
+
+if OPTIONS[:calc_method] === 'uuid'
+  resp = Parallel.map(arr, type) {|i|
+    uuid
+  }
+else
+  resp = Parallel.map(arr, type) {|i|
+    arr
+  }
+end
 
