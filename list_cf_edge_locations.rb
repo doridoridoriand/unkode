afc354be77f96599dc7391452fbd1248b509d576
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-04-13 16:30:37 +0900

    Loggerのインスタンスを２つ保持していたが１つで大丈夫なのでそのように修正

--- list_cf_edge_locations.rb
+++ list_cf_edge_locations.rb
@@ -4,7 +4,6 @@ require 'pry'
 
 agent                  = Mechanize.new
 agent.log              = Logger.new STDOUT
-logger                 = Logger.new STDOUT
 agent.user_agent_alias = 'Mac Safari'
 
 page              = agent.get('http://www.jpsgl.com/code-search')
@@ -16,7 +15,7 @@ rows_html_element = airport_codes_raw.first.children[1]
 airports_rough_data = rows_html_element.children.map {|row|
   if row.name == 'tr'
     row.children.map {|r|
-      logger.info("#{r.children.text} parsed.")
+      agent.log.info("#{r.children.text} parsed.")
       r.children.text if r.name != 'text'
     }
   end
