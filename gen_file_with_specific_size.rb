bf295d431c387c9c5a962f11bfe6c9227ed82b49
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-05-03 11:23:46 +0900

    作成するファイルが物理ディスクの容量を超えないようにするバリデーションを作成中

--- gen_file_with_specific_size.rb
+++ gen_file_with_specific_size.rb
@@ -1,15 +1,23 @@
 require 'uuid'
+require 'optparse'
 require 'securerandom'
+require 'sys-filesystem'
 require 'pry'
 
 OPTIONS = {}
 OptionParser.new do |opt|
-  opt.on('-s size', '--size', Integer) {|v| OPTIONS[:size] = v}
-  opt.on('-n filename', '--filename', String) {|v| OPTIONS[:filename] = v}
+  opt.on('-s size', '--size',                                      Integer) {|v| OPTIONS[:size]           = v}
+  opt.on('-o output-directory', 'Output Directory with Full path', String)  {|v| OPTIONS[:directory_path] = v}
+  opt.on('-n filename', '--filename',                              String)  {|v| OPTIONS[:filename]       = v}
   opt.parse!(ARGV)
 end
 
 # ファイルサイズはGB指定
-raise OptionParser::MissingArgument, 'FileSizeNotSprcified' unless OPTIONS[:size]
-raise OptionParser::MissingArgument, 'FileNameNotSpecified' unless OPTIONS[:filename]
+raise OptionParser::MissingArgument, 'FileSizeNotSprcified'      unless OPTIONS[:size]
+raise OptionParser::MissingArgument, 'DirectoryPathNotSpecified' unless OPTIONS[:directory_path]
+raise OptionParser::MissingArgument, 'FileNameNotSpecified'      unless OPTIONS[:filename]
 
+stat = Sys::Filesystem.stat(OPTIONS[:directory_path])
+p stat.blocks_free * stat.block_size
+
+binding.pry
