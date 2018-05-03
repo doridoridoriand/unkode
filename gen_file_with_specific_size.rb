4524b06cfce7883e55ac795b8256bb476a1e579e
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-05-03 22:27:52 +0900

    例外処理を作成中

--- gen_file_with_specific_size.rb
+++ gen_file_with_specific_size.rb
@@ -8,11 +8,21 @@ require 'pry'
 class FilesizeExceedsTargetDriveError < StandardError
 end
 
+class FilesizeReachedError < StandardError
+  raise StandardError
+  exit 1
+end
+
+class FileAlreadyExistsError < StandardError
+  raise StandardError
+  exit 1
+end
+
 OPTIONS = {}
 OptionParser.new do |opt|
-  opt.on('-s size', 'File size(GB)',                               Integer) {|v| OPTIONS[:size]           = v}
-  opt.on('-o output-directory', 'Output Directory with Full path', String)  {|v| OPTIONS[:directory_path] = v}
-  opt.on('-n filename', '--filename',                              String)  {|v| OPTIONS[:filename]       = v}
+  opt.on('-s size', 'File size(GB)',                             Integer) {|v| OPTIONS[:size]           = v}
+  opt.on('-d directory-path', 'Output Directory with Full path', String)  {|v| OPTIONS[:directory_path] = v}
+  opt.on('-f filename', '--filename',                            String)  {|v| OPTIONS[:filename]       = v}
   opt.parse!(ARGV)
 end
 
@@ -23,6 +33,25 @@ raise OptionParser::MissingArgument, 'FileNameNotSpecified'      unless OPTIONS[
 stat = Sys::Filesystem.stat(OPTIONS[:directory_path])
 available_gigabytes = stat.blocks_free * stat.block_size / 1024 / 1024 / 1024
 
+# 指定した保存ディレクトリのディスク容量が指定したファイルサイズ以下だった場合、例外
 raise FilesizeExceedsTargetDriveError unless available_gigabytes > OPTIONS[:size]
 
-#binding.pry
+absolute_file_path = "#{OPTIONS[:directory_path]}/#{OPTIONS[:filename]}.txt"
+
+# 既にファイルが指定したディレクトリに存在する場合、例外とする
+if Dir.exist?(absolute_file_path)
+  raise FileAlreadyExistsError
+end
+
+while true
+  begin
+    File.open(absolute_file_path, 'a') do |r|
+      r << SecureRandom.hex(256)
+      if File.size(absolute_file_path) >= OPTIONS[:size] * 1024 * 1024 * 1024
+        raise FilesizeReachedError
+      end
+    end
+  rescue => e
+  end
+end
+
