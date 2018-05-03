2e9c2b60952a3dd684b3db7c8c2274a61696ffce
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-05-03 14:51:25 +0900

    独自例外クラスを追加

--- gen_file_with_specific_size.rb
+++ gen_file_with_specific_size.rb
@@ -4,20 +4,25 @@ require 'securerandom'
 require 'sys-filesystem'
 require 'pry'
 
+# ちゃんと独自例外内部で補足するようにしたい
+class FilesizeExceedsTargetDriveError < StandardError
+end
+
 OPTIONS = {}
 OptionParser.new do |opt|
-  opt.on('-s size', '--size',                                      Integer) {|v| OPTIONS[:size]           = v}
+  opt.on('-s size', 'File size(GB)',                               Integer) {|v| OPTIONS[:size]           = v}
   opt.on('-o output-directory', 'Output Directory with Full path', String)  {|v| OPTIONS[:directory_path] = v}
   opt.on('-n filename', '--filename',                              String)  {|v| OPTIONS[:filename]       = v}
   opt.parse!(ARGV)
 end
 
-# ファイルサイズはGB指定
 raise OptionParser::MissingArgument, 'FileSizeNotSprcified'      unless OPTIONS[:size]
 raise OptionParser::MissingArgument, 'DirectoryPathNotSpecified' unless OPTIONS[:directory_path]
 raise OptionParser::MissingArgument, 'FileNameNotSpecified'      unless OPTIONS[:filename]
 
 stat = Sys::Filesystem.stat(OPTIONS[:directory_path])
-p stat.blocks_free * stat.block_size
+available_gigabytes = stat.blocks_free * stat.block_size / 1024 / 1024 / 1024
 
-binding.pry
+raise FilesizeExceedsTargetDriveError unless available_gigabytes > OPTIONS[:size]
+
+#binding.pry
