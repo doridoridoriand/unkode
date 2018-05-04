e1966d65769100937deff5e516d3ca088c311b9e
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-05-04 13:49:43 +0900

    独自例外を削除。ディスクの空き容量の計算がおかしかったので修正

--- gen_file_with_specific_size.rb
+++ gen_file_with_specific_size.rb
@@ -4,20 +4,6 @@ require 'securerandom'
 require 'sys-filesystem'
 require 'pry'
 
-# ちゃんと独自例外内部で補足するようにしたい
-class FilesizeExceedsTargetDriveError < StandardError
-end
-
-class FilesizeReachedError < StandardError
-  raise StandardError
-  exit 1
-end
-
-class FileAlreadyExistsError < StandardError
-  raise StandardError
-  exit 1
-end
-
 OPTIONS = {}
 OptionParser.new do |opt|
   opt.on('-s size', 'File size(GB)',                             Integer) {|v| OPTIONS[:size]           = v}
@@ -31,24 +17,27 @@ raise OptionParser::MissingArgument, 'DirectoryPathNotSpecified' unless OPTIONS[
 raise OptionParser::MissingArgument, 'FileNameNotSpecified'      unless OPTIONS[:filename]
 
 stat = Sys::Filesystem.stat(OPTIONS[:directory_path])
-available_gigabytes = stat.blocks_free * stat.block_size / 1024 / 1024 / 1024
+available_gigabytes = stat.bytes_free / 1024 / 1024 / 1024
 
 # 指定した保存ディレクトリのディスク容量が指定したファイルサイズ以下だった場合、例外
-raise FilesizeExceedsTargetDriveError unless available_gigabytes > OPTIONS[:size]
+raise StandardError, 'FilesizeExceedsTargetDriveError' unless available_gigabytes > OPTIONS[:size]
 
 absolute_file_path = "#{OPTIONS[:directory_path]}/#{OPTIONS[:filename]}.txt"
+binding.pry
 
 # 既にファイルが指定したディレクトリに存在する場合、例外とする
-if Dir.exist?(absolute_file_path)
-  raise FileAlreadyExistsError
+if File.file?(absolute_file_path)
+  raise StandardError, 'FileAlreadyExistsError'
 end
+
+binding.pry
 
 while true
   begin
     File.open(absolute_file_path, 'a') do |r|
       r << SecureRandom.hex(256)
       if File.size(absolute_file_path) >= OPTIONS[:size] * 1024 * 1024 * 1024
-        raise FilesizeReachedError
+        raise StandardError, 'FileSizeReachedError'
       end
     end
   rescue => e
