f3d9f45485b59e845f91823ec1839f41a86b739c
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-05-12 14:08:54 +0900

    Python2系でも動いてほしかったのと手っ取り早く対応したかったので、英語のフィードから取って英単語条件でマッチするように変更した

--- check_latest_ruby_version.py
+++ check_latest_ruby_version.py
@@ -9,12 +9,12 @@ def pretty_print(obj):
 def versions(obj):
     ver = []
     for entry in obj:
-        if 'リリース' in entry:
+        if 'Released' in entry:
             ver.append(entry)
     return re.findall('[0-9].[0-9].[0-9]', ','.join(ver))
 
 if __name__ == '__main__':
-    RSS_URL = 'https://www.ruby-lang.org/ja/feeds/news.rss'
+    RSS_URL = 'https://www.ruby-lang.org/en/feeds/news.rss'
     entries = feedparser.parse(RSS_URL).entries
     titles = [obj.title for obj in entries]
     print(versions(titles)[0])
