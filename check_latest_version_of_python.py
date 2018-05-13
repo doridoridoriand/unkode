04602ffc12efaec7db46a17382bbe775a8bb3c52
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-05-13 16:46:11 +0900

    BeautifulSoup慣れない

--- check_latest_version_of_python.py
+++ check_latest_version_of_python.py
@@ -6,3 +6,12 @@
 import urllib2
 from bs4 import BeautifulSoup
 
+import pdb
+
+html = urllib2.urlopen('https://www.python.org/downloads/')
+soup = BeautifulSoup(html, 'html.parser')
+
+li = soup.find_all('li')
+
+
+pdb.set_trace()
