73b5f76a8f09fd8b26d6b8b52b3ec7a65a4a72dc
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-06-08 18:42:23 +0900

    ひとまず起動しているコンテナのCPU使用量を取れた(単位が不明だけれども)

--- get_docker_container_stats/total_cpu_usage.py
+++ get_docker_container_stats/total_cpu_usage.py
@@ -2,12 +2,23 @@ import docker
 import pdb
 import pprint
 
+client = docker.from_env()
+
 def pretty_print(obj):
     pprint.pprint(obj)
 
+
+def containers():
+    """
+    @return list
+    """
+    return client.containers.list()
+
 if __name__ == '__main__':
-    client = docker.from_env()
-    containers = client.containers.list()
-    for containers in containers:
-        client.containers.get(client.containers.list()[0].id).attrs
-        pdb.set_trace()
+    containers = containers()
+    useage_in_usermode = []
+    for container in containers:
+        container = client.containers.get(container.id)
+        useage_in_usermode.append(container.stats(stream=False)['precpu_stats']['cpu_usage']['usage_in_usermode'])
+
+    print(pretty_print(useage_in_usermode))
