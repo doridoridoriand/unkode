15bae19734ecf83f65d573ce3536ce2debb747bc
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-06-09 21:22:20 +0900

    statからひとまず色々必要な値を引き出している

--- get_docker_container_stats/total_cpu_usage.py
+++ get_docker_container_stats/total_cpu_usage.py
@@ -2,11 +2,17 @@ import docker
 import pdb
 import pprint
 
+NANOCPUS_SCALE = 1000000000
 client = docker.from_env()
 
 def pretty_print(obj):
     pprint.pprint(obj)
 
+def container_attr(container_id):
+    return client.containers.get(container_id)
+
+def container_status(container, stream_mode=False):
+    return container.stats(stream=stream_mode)
 
 def containers():
     """
@@ -18,7 +24,17 @@ if __name__ == '__main__':
     containers = containers()
     useage_in_usermode = []
     for container in containers:
-        container = client.containers.get(container.id)
+        container        = container_attr(container.id)
+        container_status = container_status(container)
+
+        container_name     = container_status['name']
+        total_cpu_usage    = container_status['cpu_stats']['cpu_usage']['total_usage']
+        availabe_cpu_usage = container_status['cpu_stats']['cpu_usage']['usage_in_usermode']
+        online_cpus        = container_status['cpu_stats']['online_cpus']
+        system_cpu_usage   = container_status['cpu_stats']['system_cpu_usage']
+
         useage_in_usermode.append(container.stats(stream=False)['precpu_stats']['cpu_usage']['usage_in_usermode'])
+        pretty_print(container.stats(stream=False))
+        pdb.set_trace()
 
     print(pretty_print(useage_in_usermode))
