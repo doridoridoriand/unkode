7dbf47d6735fa44fbc41bd0d2dbde79e52a8558e
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-07-01 16:09:55 +0900

    実行時に必要とするパスを追加した

--- import_cf_logs_into_mysql/lib/base.rb
+++ import_cf_logs_into_mysql/lib/base.rb
@@ -2,9 +2,14 @@ require 'bundler'
 Bundler.require
 
 $: << File.expand_path(File.join(__FILE__, '..'))
+$: << File.expand_path(File.join(__FILE__, 'domain'))
+$: << File.expand_path(File.join(__FILE__, 'infrastructure'))
 require 'validator'
+require 'mysql_connector'
+require 'redis_connector'
 
 include Validator
 
 config = YAML.load_file(File.expand_path(File.join(__FILE__, '..', '..', 'config', 'config.yml')))
 config.mysql_directive_exists?
+config.redis_directive_exists?
