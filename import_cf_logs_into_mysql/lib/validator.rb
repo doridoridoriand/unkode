2dc9213b9db8f3d59defedad2f5e91203a25326b
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-07-01 16:10:17 +0900

    redisのディレクティブも必要とするようにした

--- import_cf_logs_into_mysql/lib/validator.rb
+++ import_cf_logs_into_mysql/lib/validator.rb
@@ -1,7 +1,17 @@
 module Validator
 
+  # mysqlディレクティブが存在するか否かをチェックする
+  # @return boolean
   def mysql_directive_exists?
     raise StandardError, 'MySQLDirectiveNotExistError' unless self['mysql']
+    true
+  end
+
+  # redisディレクティブが存在するか否かをチェックする
+  # @return boolean
+  def redis_directive_exists?
+    raise StandardError, 'RedisDirectiveNotExistError' unless self['redis']
+    true
   end
 
 end
