42a1696da6b97eef26f0b3acf9ba8be6978307d0
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-07-01 15:09:46 +0900

    mysqlディレクティブがなければ例外を吐くようにした

--- import_cf_logs_into_mysql/lib/base.rb
+++ import_cf_logs_into_mysql/lib/base.rb
@@ -4,5 +4,7 @@ Bundler.require
 $: << File.expand_path(File.join(__FILE__, '..'))
 require 'validator'
 
-config = YAML.load_file(File.expand_path(File.join(__FILE__, '..', '..', 'config', 'config.yml')))
+include Validator
 
+config = YAML.load_file(File.expand_path(File.join(__FILE__, '..', '..', 'config', 'config.yml')))
+config.mysql_directive_exists?
--- import_cf_logs_into_mysql/lib/validator.rb
+++ import_cf_logs_into_mysql/lib/validator.rb
@@ -1,6 +1,7 @@
 module Validator
 
   def mysql_directive_exists?
+    raise StandardError, 'MySQLDirectiveNotExistError' unless self['mysql']
   end
 
 end
