6f8d5e9207d661c01c99aca4b2ab259cf815045b
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-07-02 21:18:14 +0900

    ログをインサートするところまで作った

--- import_cf_logs_into_mysql/import_cf_log_to_mysql.rb
+++ import_cf_logs_into_mysql/import_cf_log_to_mysql.rb
@@ -5,10 +5,12 @@ require 'zlib'
 require 'pry'
 
 OPTIONS = {}
+TABLE_NAME = 'cflog'
 OptionParser.new do |opt|
   opt.on('-y yaml-file-path', 'Absolute directory path of configure file', String) {|v| OPTIONS[:yaml_file_path]   = v}
   opt.on('-d directory-path', 'Absolute directory path of log files',      String) {|v| OPTIONS[:directory_path]   = v}
   opt.on('-c',  '--create-db-schema')                                              {|v| OPTIONS[:create_db_schema] = v}
+  opt.on('-i',  '--insert')                                                        {|v| OPTIONS[:insert]           = v}
   opt.parse!
 end
 
@@ -20,10 +22,10 @@ mysql_config = YAML.load_file(OPTIONS[:yaml_file_path])['mysql']
 @log_file_pathes = Dir.glob("#{OPTIONS[:directory_path]}/*")
 
 
-@client = Mysql2::Client.new(:host =>   mysql_config['host'],
-                           :username => mysql_config['username'],
-                           :password => mysql_config['password'],
-                           :database => mysql_config['database'])
+@client = Mysql2::Client.new(:host =>     mysql_config['host'],
+                             :username => mysql_config['username'],
+                             :password => mysql_config['password'],
+                             :database => mysql_config['database'])
 
 public
 
@@ -32,9 +34,7 @@ def create_schema
     rows   = @log_file_pathes.sample(1).first.log_contents
     fields = rows.to_arrays[1].first.split(' ')
     fields.shift
-    fields
-    table_name = 'cflog'
-    query = "create table `#{table_name}` ("
+    query = "create table `#{TABLE_NAME}` ("
     query << "id bigint(20) unsigned not null auto_increment,"
     fields.map {|r|
       query << "`#{r}` varchar(255) not null,"
@@ -46,12 +46,22 @@ def create_schema
       true
     end
   rescue => e
-    raise e.exception("an error occurs during create_table. error message: #{e.message}")
+    raise e.exception("An error occurs during create_table. Error message: #{e.message}")
   end
 end
 
 def insert_row
+  rows   = @log_file_pathes.sample(1).first.log_contents
+  column = rows.to_arrays[1].first.split(' ')
+  column.shift
+  column = column.map {|r| "`#{r}`"}.join(',')
 
+  rows   = @log_file_pathes.sample(10).map {|r| r.log_contents}
+  fields = rows.map {|r| r.to_arrays}.map {|r| r.map {|f| f if f.count > 1}.compact}
+  fields.map {|file| file.map {|r|
+    q = "insert into #{TABLE_NAME}(#{column}) values(#{r.map {|f| "'#{f}'"}.join(',')})"
+    @client.query(q)
+  }}
 end
 
 def log_contents
@@ -65,3 +75,6 @@ end
 #テーブルの存在を確認する。なければcreate_schemaを実行
 tables = @client.query("show tables").map {|r| r}
 create_schema if OPTIONS[:create_db_schema] && tables.size == 0
+
+tables = @client.query("show tables").map {|r| r}
+insert_row if OPTIONS[:insert] && tables.size != 0
