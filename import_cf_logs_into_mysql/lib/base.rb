d85f2d53d7be44b49f6967b626675ff20b01109c
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-07-07 20:59:36 +0900

    helperクラスを用意した

--- import_cf_logs_into_mysql/lib/base.rb
+++ import_cf_logs_into_mysql/lib/base.rb
@@ -2,14 +2,23 @@ require 'bundler'
 Bundler.require
 
 $: << File.expand_path(File.join(__FILE__, '..'))
-$: << File.expand_path(File.join(__FILE__, 'domain'))
-$: << File.expand_path(File.join(__FILE__, 'infrastructure'))
+$: << File.expand_path(File.join(__FILE__, '..', 'domain'))
+$: << File.expand_path(File.join(__FILE__, '..', 'infrastructure'))
+$: << File.expand_path(File.join(__FILE__, '..', 'helper'))
 require 'validator'
+
+# Infrastructure
 require 'mysql_connector'
 require 'redis_connector'
 
+# Helper
+require 'dwh_enum'
+
+# Domain
+require 'insert'
+
 include Validator
 
-config = YAML.load_file(File.expand_path(File.join(__FILE__, '..', '..', 'config', 'config.yml')))
-config.mysql_directive_exists?
-config.redis_directive_exists?
+@config = YAML.load_file(File.expand_path(File.join(__FILE__, '..', '..', 'config', 'config.yml')))
+@config.mysql_directive_exists?
+@config.redis_directive_exists?
