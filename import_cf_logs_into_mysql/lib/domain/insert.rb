3ec8b829f3abeb8b64c57e8f3685ba9a873e5639
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-07-07 21:00:12 +0900

    DWHを選択選択する部分を作成中

--- import_cf_logs_into_mysql/lib/domain/insert.rb
+++ import_cf_logs_into_mysql/lib/domain/insert.rb
@@ -1,22 +1,38 @@
 class Insert
   attr :config, :dwh
- 
+
+  # データーストアが何を選択されているかを保持する
+  @@dwh_flag = ''
+
   def initialize(config, dwh)
     mysql_config = config['mysql']
     redis_config = config['redis']
     @dwh         = dwh
     if mysql_config != nil
-      @mysql_client = MysqlConnector.new(@mysql_config, schema)
+      mysql_client = MysqlConnector.new(mysql_config)
     end
 
     if redis_config != nil
-      @mysql_client = RedisConnector.new(@mysql_config, schema)
+      redis_client = RedisConnector.new(redis_config)
     end
   end
 
   # DWHの選択をする
-  def dwh
-    binding.pry
+  def dwh(mode)
+  end
+
+  def insert(data)
+  end
+
+  private
+
+  def __dwh_set
+  end
+
+  def __dwh_get_current_setting
+  end
+
+  def __dwh_remove_current_setting
   end
 
 end
