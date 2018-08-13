a95314b296244a080e2b7c134d5a8c04dc268043
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-08-13 10:05:44 +0900

    PHPわすれた

--- gen_file_with_specific_size/gen_file_with_specific_size.php
+++ gen_file_with_specific_size/gen_file_with_specific_size.php
@@ -10,6 +10,11 @@ function hex128() {
   return mb_strimwidth($str, 0, 128);
 }
 
+# 配列の番号を書けば、そこの値を返す関数
+function options($obj, $index) {
+  return $obj[$index];
+}
+
 $arguments = [
   # [short option, long option, required or not, discript]
   ['s', 'size',           true, 'File size(GB)'],
@@ -17,11 +22,11 @@ $arguments = [
   ['d', 'directory-path', true, 'Output directory with full path']
 ];
 
-eval(\Psy\sh());
-
-$short_options = 
-$long_options  = 
+#$short_options = foreach ($argumetns)
+#$long_options  = 
 $options = getopt($short_options, $long_options);
+
+eval(\Psy\sh());
 
 # 全ての項目が存在するか自前でバリデーションを作らないといけない
 $short_options_split = explode(':', $short_options);
