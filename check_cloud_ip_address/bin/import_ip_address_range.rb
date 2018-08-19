a934fc6ca6b59c44da3ea451b738a12f441d30ff
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-08-19 22:14:58 +0900

    書き途中

--- check_cloud_ip_address/bin/import_ip_address_range.rb
+++ check_cloud_ip_address/bin/import_ip_address_range.rb
@@ -6,4 +6,18 @@ require 'pry'
 
 ip_range_source = YAML.load_file(File.join(__dir__, '..', 'config', 'ip_range.yml'))
 
-binding.pry
+def normal(vendor_information)
+  binding.pry
+end
+
+def wget(vendor_information)
+end
+
+def dig(vendor_information)
+end
+
+ip_range_source.first['address_range_list'].map {|vendor|
+  binding.pry
+  send(vendor.values.flatten.first['gettype'], vendor)
+}
+
