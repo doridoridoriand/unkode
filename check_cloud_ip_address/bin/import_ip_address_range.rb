969a8a8b0ffe4a37c561c0b05087ad57a73eaac8
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-08-21 09:11:51 +0900

    共通化できる部分を共通化した。レシーバーからの結果をhashに保存するようにした

--- check_cloud_ip_address/bin/import_ip_address_range.rb
+++ check_cloud_ip_address/bin/import_ip_address_range.rb
@@ -1,6 +1,7 @@
 require 'systemu'
 require 'open-uri'
 require 'json'
+require 'rexml/document'
 require 'ipaddress'
 require 'parallel'
 require 'yaml'
@@ -9,22 +10,36 @@ require 'pry'
 
 ip_range_source = YAML.load_file(File.join(__dir__, '..', 'config', 'ip_range.yml'))
 
+public
+
 def aws(vendor_information)
   file = open(vendor_information.values.flatten.first['uri']).read
-  cidrs = JSON.parse(file)['prefixes']
-  ips = Parallel.map(cidrs.map {|r| IPAddress r['ip_prefix']}) do |cidr|
-    cidr.map {|ip| ip.octets.join('.')}
-  end
-  ips.flatten
+  res = JSON.parse(file)['prefixes']
+  cidrs = res.map {|r| r['ip_prefix']}
+  cidrs.ip_lists
 end
 
 def azure(vendor_information)
+  file = open(vendor_information.values.flatten.first['uri'].split('//').last).read
+  xml_source = REXML::Document.new(file)
+  res = xml_source.elements.each('/AzurePublicIpAddresses/Region/IpRange') do |elem|
+    elem
+  end
+  cidrs = res.map {|r| r['Subnet']}
+  cidrs.ip_lists
 end
 
 def gcp(vendor_information)
 end
 
-ip_range_source.first['address_range_list'].map {|vendor|
-  send(vendor.keys.first, vendor)
+def ip_lists
+  ips = Parallel.map(self.map {|r| IPAddress r}) do |cidr|
+    cidr.map {|ip| ip.octets.join('.')}
+  end
+  ips.flatten
+end
+
+vendor_ips = ip_range_source.first['address_range_list'].map {|vendor|
+  {vendor.keys.first.to_sym => send(vendor.keys.first, vendor)}
 }
 
