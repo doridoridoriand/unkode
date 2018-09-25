83f5a78d623a5f18d807d506b96e954c2ee07d84
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-09-25 11:24:28 +0900

    コマンドライン引数の部分中途半端だったので書いた

--- gen_file_with_specific_size/gen_file_with_specific_size.js
+++ gen_file_with_specific_size/gen_file_with_specific_size.js
@@ -1,10 +1,33 @@
 let argv = require('argv');
+let crypto = require('crypto');
 
 argv.option({
-  name: 'size',
-  short: 's',
-  type : 'integer',
-  description :'File size(GB)',
-  example: "'gen_file_with_specific_size.js --size 1' or 'gen_file_with_specific_size.js -s 1'"
+  name:        'size',
+  short:       's',
+  type:        'integer',
+  description: 'File size(GB)',
+  example:     '--size=10 or -s 10'
 });
-console.log(argv.run());
+argv.option({
+  name:        'directory-path',
+  short:       'd',
+  type:        'string',
+  description: 'Please input absolute directory path.',
+  example:     '--directory-path=/dev/null or -d /dev/null'
+});
+argv.option({
+  name:        'filename',
+  short:       'f',
+  type:        'string',
+  description: 'File name',
+  example:     '--filename=hex_10GB.txt or -f hex_10GB.txt'
+});
+arguments = argv.run();
+
+console.log(arguments);
+
+function hex128() {
+  // なんかtoString('hex')で変換すると256文字帰ってくるので半分の64bytesで生成している
+  return crypto.randomBytes(64).toString('hex');
+}
+
