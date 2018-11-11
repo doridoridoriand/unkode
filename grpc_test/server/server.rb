951587166ece03fd0b28eecaa34cc2cbc661da2a
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-11-11 11:34:32 +0900

    設定情報を外出しした

--- grpc_test/server/server.rb
+++ grpc_test/server/server.rb
@@ -1,10 +1,12 @@
 $: << File.expand_path(File.join(__dir__))
 require'bundler'
+require 'yaml'
 require 'server_base'
 Bundler.require
 
-server = GRPC::RpcServer.new
-server.add_http2_port('localhost:50051', :this_port_is_insecure)
-server.handle(TestServer)
+config = YAML.load_file(File.join(__dir__, '..', 'config', 'server.yml'))['server']
 
+server = GRPC::RpcServer.new
+server.add_http2_port("#{config['host']}:#{config['port']}", :this_port_is_insecure)
+server.handle(TestServer)
 server.run_till_terminated
