2731607caaddc9b3577de5df5abf9c2ae0cbc217
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-11-11 17:46:05 +0900

    オウム返しだとつまらないのでもう少し処理して返すことにした(現在時刻返すだけだけれども)

--- grpc_test/server/server_base.rb
+++ grpc_test/server/server_base.rb
@@ -1,11 +1,12 @@
 $: << File.expand_path(File.join(__dir__, '..', 'lib'))
 require 'bundler'
 Bundler.require
+require 'time'
 require 'test_services_pb'
 
 class TestServer < Helloworld::Greeter::Service
 
   def say_hello(hello_req, _unused_call)
-    Helloworld::HelloReply.new(message: "Hello #{hello_req.name}")
+    Helloworld::HelloReply.new(message: "Hello #{hello_req.name}. #{Time.now.to_s}")
   end
 end
