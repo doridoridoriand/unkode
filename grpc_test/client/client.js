5166040b1cffd6de474512b9093cf9a32892c211
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-11-11 17:45:16 +0900

    取り合えず出来た

--- grpc_test/client/client.js
+++ grpc_test/client/client.js
@@ -6,14 +6,16 @@ const argv = require('argv');
 const protoLoader = require('@grpc/proto-loader');
 
 argv.option({
-  name:  'username',
-  short: 'u',
-  type:  'string',
+  name:        'username',
+  short:       'u',
+  type:        'string',
   description: 'Please input some username.',
-  example: '--username=doridoridoriand or -s doridoridoriand'
+  example:     '--username=doridoridoriand or -s doridoridoriand'
 });
 
 const arguments = argv.run();
+
+if (!arguments.options.username) { console.error('NoUsernameDetectedError'); process.exit(1); }
 
 const config_filepath = [__dirname, '..', 'config', 'client.yml'].join('/');
 const config = yaml.safeLoad(fs.readFileSync(config_filepath, 'utf8'))['endpoint'];
@@ -25,5 +27,10 @@ const packageDefinition = protoLoader.loadSync(
 );
 
 const test_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+const client = new test_proto.Greeter([config['host'], config['port']].join(':'),
+                                      grpc.credentials.createInsecure());
 
-debugger;
+client.sayHello({name: arguments.options.username}, function(err, response) {
+  console.log(response.message);
+});
+
