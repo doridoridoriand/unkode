dcd27a30c72219e70175a39ccecbe2f67ca8aed2
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-11-11 17:22:26 +0900

    コマンドライン引数取れるようにした

--- grpc_test/client/client.js
+++ grpc_test/client/client.js
@@ -1,8 +1,19 @@
 const grpc = require('grpc');
 const yaml = require('js-yaml');
 const fs   = require('fs');
+const argv = require('argv');
 
 const protoLoader = require('@grpc/proto-loader');
+
+argv.option({
+  name:  'username',
+  short: 'u',
+  type:  'string',
+  description: 'Please input some username.',
+  example: '--username=doridoridoriand or -s doridoridoriand'
+});
+
+const arguments = argv.run();
 
 const config_filepath = [__dirname, '..', 'config', 'client.yml'].join('/');
 const config = yaml.safeLoad(fs.readFileSync(config_filepath, 'utf8'))['endpoint'];
