b9c8107bbdba853c2016a75b9f361141b0b991c4
doridoridoriand <i.am.eager.for.peace@gmail.com>
2019-05-08 13:32:58 +0900

    iTunesのAPI叩いてみた

---
+++ itunes.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'open-uri'
+require 'json'
+
+res = JSON.parse(open('https://itunes.apple.com/search?term=kinggnu/').map {|r| r}.join)
+search_results = res['results']
+puts search_results.select {|r| r['trackName'] === 'Hakujitsu'}.first['collectionViewUrl']
