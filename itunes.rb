c853b6ed1415174d69d7137bc14f390ea69034bb
doridoridoriand <i.am.eager.for.peace@gmail.com>
2019-05-08 13:41:14 +0900

    シングルクォーテーションをダブルに変更した(だけ)

--- itunes.rb
+++ itunes.rb
@@ -1,7 +1,6 @@
-require 'pry'
-require 'open-uri'
-require 'json'
+require "open-uri"
+require "json"
 
-res = JSON.parse(open('https://itunes.apple.com/search?term=kinggnu/').map {|r| r}.join)
-search_results = res['results']
-puts search_results.select {|r| r['trackName'] === 'Hakujitsu'}.first['collectionViewUrl']
+res = JSON.parse(open("https://itunes.apple.com/search?term=kinggnu/").map {|r| r}.join)
+search_results = res["results"]
+puts search_results.select {|r| r["trackName"] === "Hakujitsu"}.first["collectionViewUrl"]
