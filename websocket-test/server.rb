377b2425fe91b4e20ff1e946a97b42bf8f566cdf
doridoridoriand <i.am.eager.for.peace@gmail.com>
2019-07-14 16:11:51 +0900

    ほぼREADME通りのコードを書いた

--- websocket-test/server.rb
+++ websocket-test/server.rb
@@ -1,3 +1,24 @@
 require 'bundler'
 Bundler.require
 
+App = lambda do |env|
+  if Faye::WebSocket.websocket?(env)
+    ws = Faye::WebSocket.new(env)
+
+    ws.on :message do |event|
+      ws.send(event.data)
+    end
+
+    ws.on :close do |event|
+      p [:close, event.code, event.reason]
+      ws = nil
+    end
+
+    # Return async Rack response
+    ws.rack_response
+
+  else
+    # Normal HTTP request
+    [200, { 'Content-Type' => 'application/json' }, [JSON.dump({'status': 200})]]
+  end
+end
