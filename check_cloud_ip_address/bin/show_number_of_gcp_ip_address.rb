9c2c0345d31108466548ff8108049669862dfde1
doridoridoriand <i.am.eager.for.peace@gmail.com>
2019-09-28 18:13:42 +0900

    GCPのやつも追加

---
+++ check_cloud_ip_address/bin/show_number_of_gcp_ip_address.rb
@@ -0,0 +1,34 @@
+require 'parallel'
+require 'systemu'
+require 'ipaddress'
+require 'open-uri'
+require 'logger'
+require 'pry'
+
+public
+
+logger = Logger.new STDOUT
+logger.info('Start to parse IP Address ranges of GCP.')
+
+pid, res = systemu "dig -t txt _spf.google.com +short"
+netblocks_txt = res.split(' ').map {|r|
+  r if r.include?('include')
+}.compact.map {|r|
+  r.split(':').last
+}
+cidrs = netblocks_txt.map {|r|
+  binding.pry
+  pid, res = systemu "dig -t txt #{r} +short"
+  res.split(' ').map {|r|
+    r if r.include?('ip4')
+  }.compact
+}.flatten.map {|r| r.split(':').last}
+
+logger.info("Number of CIDR blocks: #{cidrs.count}")
+
+ips = Parallel.map(cidrs.map {|r| IPAddress r}) do |cidr|
+  cidr.map {|ip| ip.octets.join('.')}
+end
+ips.flatten
+
+logger.info("Completed to calculate number of IP addresses. Number of IP address of GCP: #{ips.flatten.count}")
