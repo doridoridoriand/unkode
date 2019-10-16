28e54154c1654bfa513c66e5589040cef3b52a90
doridoridoriand <i.am.eager.for.peace@gmail.com>
2019-10-16 15:02:36 +0900

    ひとまず動くようにした

---
+++ simple_chat/server/main.js
@@ -0,0 +1,18 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+let port = 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
