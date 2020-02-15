469ea706f171068359af6019627256bf941f780d
doridoridoriand <i.am.eager.for.peace@gmail.com>
2020-02-16 00:52:06 +0900

    multipassを使ってVM作るシェル書いた

---
+++ vm_with_multipass/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# このシェルはMacOSでしか動きません
+
+# 仮想マシン名
+vmName=ubuntu01
+
+# CPUはホストの全コア割り当て
+numOfCPU=`system_profiler SPHardwareDataType | grep "Total Number of Cores" | grep -o '[0-9]' | tr -d '\n'`;
+
+# メモリはホストの50%割り当て
+totalMachineMemory=`system_profiler SPHardwareDataType | grep "Memory" | grep -o '[0-9]' | tr -d '\n'`;
+numOfMem=`echo $(($totalMachineMemory/2))`;
+
+# ディスク決め打ちで100GB
+numOfDisk=100;
+
+multipass launch --cpus $numOfCPU --disk "${numOfDisk}GB" --mem "${numOfMem}GB" --name $vmName;
