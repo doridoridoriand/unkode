7348508984e5f65b7330ba7cb4260273142998d2
doridoridoriand <i.am.eager.for.peace@gmail.com>
2020-05-16 21:57:55 +0900

    containersリポジトリへ移動

--- uuid-api/ruby/app.rb
+++
@@ -1,9 +0,0 @@
-Cuba.use Rack::Session::Cookie, :secret => "__a_very_long_string__"
-
-Cuba.plugin Cuba::Safe
-
-Cuba.define do
-  on root do
-    res.write JSON.dump({uuid: (0..10).to_a.map {|r| SecureRandom.uuid}})
-  end
-end
--- uuid-api/ruby/config.ru
+++
@@ -1,7 +0,0 @@
-$: << File.expand_path(File.join(['.']))
-require 'app'
-
-# 3072~4096リクエスト受け付けたらkillする
-# use Unicorn::WorkerKiller::MaxRequests, 3072, 4096
-
-run Cuba
