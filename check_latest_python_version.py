5ce66ba459dd9c5614034ec9cabdf422cd6a9379
doridoridoriand <i.am.eager.for.peace@gmail.com>
2020-07-18 22:41:38 +0900

    全然ちゃんと書いてなかったので書いた

--- check_latest_python_version.py
+++ check_latest_python_version.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python
-# coding: UTF-8
-
-# This script runs on only python2.x environment.
-
-import urllib2
+import pdb
+import pprint
+import requests
 from bs4 import BeautifulSoup
 
-import pdb
+def is_num(s):
+    return s.replace(',', '').replace('.', '').replace('-', '').replace('/', '').isnumeric()
 
-fs = urllib2.urlopen('https://www.python.org/downloads/release/latest')
-html = fs.read()
+if __name__ == '__main__':
+    RELEASES_URL = 'https://docs.python.org/release/'
+    source = requests.get(RELEASES_URL)
+    soup = BeautifulSoup(source.text, 'html.parser')
+    versions = soup.find_all('a')
+    versions_text = []
+    for v in versions:
+        if is_num(v.get_text()):
+            versions_text.append(v.get_text().replace('/', ''))
 
-soup = BeautifulSoup(html, 'html.parser')
-
-li = soup.find_all('li')
-
-
-pdb.set_trace()
+    print(sorted(versions_text)[-1])
