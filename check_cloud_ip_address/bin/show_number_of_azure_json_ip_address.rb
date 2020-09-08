8c2917fb9344fe959b1dfea4eb832600f3cf3300
doridoridoriand <i.am.eager.for.peace@gmail.com>
2020-09-08 09:43:31 +0900

    IPレンジのダウンロード先が変わっていたので修正

--- check_cloud_ip_address/bin/show_number_of_azure_json_ip_address.rb
+++ check_cloud_ip_address/bin/show_number_of_azure_json_ip_address.rb
@@ -8,10 +8,10 @@ require 'pry'
 logger = Logger.new STDOUT
 logger.info('Start to parse IP Address ranges of Azure.')
 
-JSON_ENDPOINT = {public:  'https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20190923.json',
-                 gov:     'https://download.microsoft.com/download/6/4/D/64DB03BF-895B-4173-A8B1-BA4AD5D4DF22/ServiceTags_AzureGovernment_20190923.json',
-                 germany: 'https://download.microsoft.com/download/0/7/6/076274AB-4B0B-4246-A422-4BAF1E03F974/ServiceTags_AzureGermany_20190923.json',
-                 china:   'https://download.microsoft.com/download/9/D/0/9D03B7E2-4B80-4BF3-9B91-DA8C7D3EE9F9/ServiceTags_China_20190923.json'
+JSON_ENDPOINT = {public:  'https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20200824.json',
+                 gov:     'https://download.microsoft.com/download/6/4/D/64DB03BF-895B-4173-A8B1-BA4AD5D4DF22/ServiceTags_AzureGovernment_20200824.json',
+                 germany: 'https://download.microsoft.com/download/0/7/6/076274AB-4B0B-4246-A422-4BAF1E03F974/ServiceTags_AzureGermany_20200824.json',
+                 china:   'https://download.microsoft.com/download/9/D/0/9D03B7E2-4B80-4BF3-9B91-DA8C7D3EE9F9/ServiceTags_China_20200824.json'
 }
 
 cidrs = JSON_ENDPOINT.values.map {|r|
