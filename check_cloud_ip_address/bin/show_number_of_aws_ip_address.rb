f693a91ef618e8860dfd62705001b16a204c2a29
doridoridoriand <i.am.eager.for.peace@gmail.com>
2022-08-22 12:07:02 +0900

    ログ出力の内容を少々修正

--- check_cloud_ip_address/bin/show_number_of_aws_ip_address.rb
+++ check_cloud_ip_address/bin/show_number_of_aws_ip_address.rb
@@ -12,10 +12,10 @@ file = URI.open('https://ip-ranges.amazonaws.com/ip-ranges.json').read
 res  = JSON.parse(file)['prefixes']
 cidrs = res.map {|r| r['ip_prefix']}
 
-logger.info("Number of CIDR blocks: #{cidrs.count}")
+logger.info("Number of IPv4 CIDR blocks: #{cidrs.count}")
 
 ips = Parallel.map(cidrs.map {|r| IPAddress r}) do |cidr|
   cidr.map {|ip| ip.octets.join('.')}
 end.flatten
 
-logger.info("Completed to calculate number of IP addresses. Number of IP address of AWS: #{ips.flatten.count}")
+logger.info("Completed to calculate number of IPv4 addresses. Number of IPv4 address of AWS: #{ips.flatten.count}")
