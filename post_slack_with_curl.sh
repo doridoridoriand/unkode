31aaacd2782c7aec34d6d83a501a3502da32a085
Dorian (Takahiro Ishida) <i.am.eager.for.peace@gmail.com>
2024-04-05 19:30:45 +0900

    Add script for post slack with curl

---
+++ post_slack_with_curl.sh
@@ -0,0 +1,2 @@
+curl -d "text=Hi I am a bot that can post messages to any public channel." -d "channel=C123456" -H "Authorization: Bearer xoxb-not-a-real-token-this-will-not-work" -X POST https://slack.com/api/chat.postMessage
+
