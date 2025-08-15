019b7c6998a700ee76ef49330ae92029f04d4e9f
Dorian (Takahiro Ishida) <i.am.eager.for.peace@gmail.com>
2025-08-16 02:40:36 +0900

    コミット漏れとcoderabbitの指摘事項の修正

--- get_docker_container_stats/get_all_container_cpu_usage.sh
+++ get_docker_container_stats/get_all_container_cpu_usage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 numberOfContainer=`docker ps -q | wc -l`;
-python_path=$(which python3)
+python_path=$(command -v python3) if [ -z "$python_path" ]; then echo "Error: python3 interpreter not found" >&2 exit 1 fi
 
 for ((i = 0; i < $numberOfContainer; i++)) {
   $python_path ./total_cpu_usage.py -n $i -o cpu_usage_$i.json
